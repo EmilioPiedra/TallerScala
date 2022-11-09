d23da46e0df3b599814a37e676a11fc149c0d6f5
EmilioPiedra <espiedra1@utpl.edu.ec>
2022-11-08 19:55:12 -0500

    Parte 2 de Scala:)

--- src/main/scala/app.scala
+++ src/main/scala/app.scala
@@ -1,4 +1,10 @@
+//funcion anonima ==>  val operar = (x: Int, y: Int)=> x+y es anonima porque se asigna a una variable
+//funcion nombrada ==>
+//tipos de datos basados en clases
+// case == crear un tipo de dato ==> case class Person(name: String) val p = Perosn("Jose")  p.name ==> Jose
 val operar = (x: Int, y: Int)=> x+y
 @main def app(): Unit =
   println("Hola, mundo!!!!")
   println(operar(2,5))
+  //las listas se las declaran como clases
+
